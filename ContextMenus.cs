f3c1f05d381af9dbdd9de3eeb8009f1772265dad
Ashish Kumar Shah <ashish.k.shah@oracle.com>
2016-09-24 15:11:43 +0530

    Removing the un necessary explorer menu from context

--- ContextMenus.cs
+++ ContextMenus.cs
@@ -26,13 +26,6 @@ namespace Hydrate
 			ToolStripMenuItem item;
 			ToolStripSeparator sep;
 
-			// Windows Explorer.
-			item = new ToolStripMenuItem();
-			item.Text = "Explorer";
-			item.Click += new EventHandler(Explorer_Click);
-			item.Image = Resources.Explorer;
-			menu.Items.Add(item);
-
 			// About.
 			item = new ToolStripMenuItem();
 			item.Text = "About";
@@ -54,15 +47,6 @@ namespace Hydrate
 			return menu;
 		}
 
-		/// <summary>
-		/// Handles the Click event of the Explorer control.
-		/// </summary>
-		/// <param name="sender">The source of the event.</param>
-		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
-		void Explorer_Click(object sender, EventArgs e)
-		{
-			Process.Start("explorer", null);
-		}
 
 		/// <summary>
 		/// Handles the Click event of the About control.
